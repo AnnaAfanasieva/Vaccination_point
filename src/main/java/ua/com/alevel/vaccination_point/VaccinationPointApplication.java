094560881d7f169c4a86264a7f67b7640f0cdc6f
AnnaAfanasieva <aniafanasieva24@gmail.com>
2022-06-24 17:29:40 +0200

    create repository

---
+++ src/main/java/ua/com/alevel/vaccination_point/VaccinationPointApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alevel.vaccination_point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationPointApplication.class, args);
+    }
+
+}
